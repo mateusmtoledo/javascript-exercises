cccbe2afdf0bfafd9f978f7e9a53d016ee17f216
Mateus Mauricio Toledo <mateusmtoledo1@gmail.com>
2022-02-15 18:09:23 -0300

    Solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num >=0 ) {
+        let result = '';
+        for (i = 1; i <= num; i++){
+            result += string;
+        }
+        return result;
+    }
+    else return 'ERROR';
 };
 
 // Do not edit below this line
