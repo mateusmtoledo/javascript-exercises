fe831a96542fd5ae9f508f9445412bcfdc1e13c7
Mateus Mauricio Toledo <mateusmtoledo1@gmail.com>
2022-02-15 19:07:55 -0300

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    let args = Array.from(arguments);
+    for (let i = 1; i < args.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (args[i] === array[j]) {
+                array.splice(j, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
