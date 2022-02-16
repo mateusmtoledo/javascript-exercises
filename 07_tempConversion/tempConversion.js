06a59ab34319903157a3f8969ac26f9acdc7a938
Mateus Mauricio Toledo <mateusmtoledo1@gmail.com>
2022-02-15 22:18:40 -0300

    Solve 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  return roundOnePlace((tempF-32)*5/9);
 };
 
-const ctof = function() {
+const ctof = function(tempC) {
+  return roundOnePlace(tempC*9/5+32);
+};
 
+const roundOnePlace =function(num) {
+  return Math.round(num*10)/10;
 };
 
 // Do not edit below this line
