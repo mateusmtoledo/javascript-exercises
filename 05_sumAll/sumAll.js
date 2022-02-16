158a0501fadc6d1f68093011575d0edf05301204
Mateus Mauricio Toledo <mateusmtoledo1@gmail.com>
2022-02-15 21:55:46 -0300

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (a < 0 || b < 0 || typeof(a) !== 'number' || typeof(b) !== 'number') {
+        return 'ERROR';
+    }
+    else if (a > b) {
+        let aux = a;
+        a = b;
+        b = aux;
+    }
 
+    for (let i=a; i<=b; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
