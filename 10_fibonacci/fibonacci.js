c38bb769e1ba73ed643ee7b22a9e8de9a55878b7
Mateus Mauricio Toledo <mateusmtoledo1@gmail.com>
2022-02-26 16:28:47 -0300

    Solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    const array = [1, 1];
+    for (i = 2; i < num; i++) {
+        array.push(array[1]+array[0]);
+        array.shift();
+    }
+    return array[1];
 };
 
 // Do not edit below this line
