6fee97218fa6ee76c18f9f1640a7f9de98f769b4
Mateus Mauricio Toledo <mateusmtoledo1@gmail.com>
2022-02-26 02:43:36 -0300

    Solve 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const array = [];
+    for (i = 0; i < string.length; i++) {
+        if (string.charAt(i) !== ' ' && 
+                string.charAt(i) !== '.' &&
+                string.charAt(i) !== ',' && 
+                string.charAt(i) !== '!') {
+            array.push(string.charAt(i).toLowerCase());
+        }
+    }
+    return array.every((char, index) => {
+        if (char === array[array.length - 1 - index]) return true;
+    })
 };
 
 // Do not edit below this line
