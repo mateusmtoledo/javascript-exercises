b1a0a77de06e4466b90c7908f217144ce4ea93c9
Mateus Mauricio Toledo <mateusmtoledo1@gmail.com>
2022-02-26 17:07:16 -0300

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        if(((oldest.yearOfDeath || 2022) - oldest.yearOfBirth) < ((person.yearOfDeath || 2022) - person.yearOfBirth)) {
+            return person;
+        }
+        else return oldest;
+    });
 };
 
 // Do not edit below this line
